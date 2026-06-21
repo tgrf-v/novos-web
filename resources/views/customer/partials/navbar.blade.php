b9475a400cdf2e16566da4d8d8e68a21641a3402
tegarajah24 <tegarrifai1410@gmail.com>
2026-06-21 20:29:25 +0700

    ubah icon chat di navbar

--- resources/views/customer/partials/navbar.blade.php
+++ resources/views/customer/partials/navbar.blade.php
@@ -73,7 +73,7 @@
                 {{-- Chat icon --}}
                 <a href="{{ route('chat') }}" class="p-1.5 rounded-lg hover:bg-gray-100 transition-colors" title="Chat">
                     <svg class="w-6 h-6 text-[#616161] hover:text-[#1a237e] transition-colors" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+                        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/>
                     </svg>
                 </a>
 
