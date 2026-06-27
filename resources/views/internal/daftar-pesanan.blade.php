3dd3b0ab03db32ed375c1ed827173613bdaff560
Andrea Dev <fadliandrea06@gmail.com>
2026-06-27 11:08:19 +0700

    memperlebar badge untuk Assignee di daftar pesanan

--- resources/views/internal/daftar-pesanan.blade.php
+++ resources/views/internal/daftar-pesanan.blade.php
@@ -168,7 +168,7 @@ function rupiah($n) {
                         <td class="px-5 py-4 text-gray-700 text-center whitespace-nowrap">{{ $o['qty'] }}</td>
                         <td class="px-5 py-4 text-gray-700 whitespace-nowrap font-medium">{{ rupiah($o['total']) }}</td>
                         <td class="px-5 py-4 whitespace-nowrap">
-                            <select @change="assignOrder('{{ $o['order_id'] }}', $event.target.value)" class="border border-gray-200 rounded-lg px-2 py-1.5 text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#1a237e]/30 bg-white">
+                            <select @change="assignOrder('{{ $o['order_id'] }}', $event.target.value)" class="border border-gray-200 rounded-lg px-3 py-1.5 text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#1a237e]/30 bg-white min-w-[140px]">
                                 <option value="">Unassigned</option>
                                 @foreach($assignees as $a)
                                     <option value="{{ $a['id'] }}" {{ $o['assignee_id'] == $a['id'] ? 'selected' : '' }}>
