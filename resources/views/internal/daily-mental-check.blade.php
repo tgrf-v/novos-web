85b6199e66b0fe38286f69ce6c2867a2db660397
Andrea Dev <fadliandrea06@gmail.com>
2026-06-27 11:42:17 +0700

    mengubah warna default untuk text heading pada menu daily mental check

--- resources/views/internal/daily-mental-check.blade.php
+++ resources/views/internal/daily-mental-check.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Daily Mental Check')
 
 @section('topbar-left')
-    <span class="font-bold text-gray-900">Daily Mental Check</span>
+    <h1 class="text-xl font-bold text-[#1a237e]">Daily Mental Check</h1>
 @endsection
 
 @section('internal-content')
