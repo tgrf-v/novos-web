3d984ec901c7a957c3ab358b79ccc363d72068ee
tegarajah24 <tegarrifai1410@gmail.com>
2026-07-16 12:32:22 +0700

    fix: import filepond css in app.css to ensure stylesheet is loaded globally during AJAX navigation

--- resources/js/filepond.js
+++ resources/js/filepond.js
@@ -1,8 +1,6 @@
 import * as FilePond from 'filepond'
-import 'filepond/dist/filepond.min.css'
 
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
 import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
